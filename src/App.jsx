392d7e3b75c7bfc875218f0dc5ffca628c45efa0
Sanskar Dikshit <shikharsanskar@gmail.com>
2025-03-22 16:11:55 +0530

    Initial commit for react app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { LoadingScreen } from './components/LoadingScreen';
+
+function App() {
+  return (
+    <>
+      <LoadingScreen/>
+    </>
+  );
+}
+
+export default App
